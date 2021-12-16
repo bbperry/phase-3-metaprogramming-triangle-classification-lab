9a2a50ad86cfc99acfde117cc5093f1b4a49ac28
Ben <benbperry1@gmail.com>
2021-12-15 18:42:26 -0800

    commit tree 43f088aca685cb34304f47fe9a2b6380d33a8e6f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a == b && b == c 
+      :equilateral
+    elsif a == b || a == c || b == c 
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+
 end
